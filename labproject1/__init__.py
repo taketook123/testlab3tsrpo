60b54e73b81a5e59b83c8cba024be72b11177970
taketook123 <taketook26@gmail.com>
2023-11-22 03:54:59 -0500

    add postgresql conf

--- labproject1/__init__.py
+++ labproject1/__init__.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:SomePassword@localhost/test"
-app.config['SQLALCHEMY_DATABASE_URI'] = None
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://testlab3db_q1ss_user:UMK3TtGSlxbwmixK5QPVAN20CQVsW74x@dpg-cles1ac15k1s73f5kshg-a/testlab3db_q1ss"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
